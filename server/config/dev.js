e9b731a4256ac3daed780583a34c3ee90241cfca
Alex Yu <j620656786206@gmail.com>
2018-07-04 14:12:27 -0500

    completed google oauth

---
+++ server/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '308874557752-j6ii5ao3vvlqid99faap70htdrrsnp2t.apps.googleusercontent.com',
+  googleClientSecret: 'ham21YGz60LmlgscGc4eLQFI',
+  mongoURI: 'mongodb://admin:cguavno1@ds127851.mlab.com:27851/avno1-dev',
+  cookieKey: 'gakljfhdglfshgilasdghdfasilugh'
+};
